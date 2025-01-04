e610009250a398a957723051351c8cc09c4013ad
minhnh296 <hacminh2004@gmail.com>
2025-01-04 09:45:46 +0700

    ex1

---
+++ ex1.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class ex1 {
+    private int V;
+    private LinkedList<Integer> adj[];
+
+    ex1(int v) {
+        V = v + 1;
+        adj = new LinkedList[V];
+        for (int i = 0; i < V; ++i)
+            adj[i] = new LinkedList<>();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    Boolean isReachable(int s, int d) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        boolean visited[] = new boolean[V];
+
+        visited[s] = true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+            s = queue.poll();
+
+            for (int n : adj[s]) {
+                if (n == d)
+                    return true;
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String args[]) {
+        ex1 _g = new ex1(7);
+        _g.addEdge(1, 2);
+        _g.addEdge(2, 5);
+        _g.addEdge(3, 6);
+        _g.addEdge(4, 6);
+        _g.addEdge(4, 7);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter start node: ");
+        int u = scanner.nextInt();
+        System.out.print("Enter destination node: ");
+        int v = scanner.nextInt();
+
+        if (_g.isReachable(u, v))
+            System.out.println("True");
+        else
+            System.out.println("false");
+
+        scanner.close();
+    }
+}
